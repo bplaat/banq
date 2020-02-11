852f462c18e84db756743030f1ec29f125e7fbf2
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-02-11 15:43:53 +0100

    Renamed everything to Banq

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Bank
+# Banq
 A nice bank system
--- server/config.example.php
+++ server/config.example.php
@@ -2,11 +2,11 @@
 
 define('DEBUG', true);
 
-define('DATABASE_DSN', 'mysql:host=127.0.0.1;dbname=bank');
-define('DATABASE_USER', 'bank');
+define('DATABASE_DSN', 'mysql:host=127.0.0.1;dbname=banq');
+define('DATABASE_USER', 'banq');
 define('DATABASE_PASSWORD', ''); // Your database password
 
-define('SESSION_COOKIE_NAME', 'bank-session');
+define('SESSION_COOKIE_NAME', 'banq-session');
 define('SESSION_DURATION', 60 * 60 * 24 * 356);
 
 define('USER_ROLE_NORMAL', 1);
