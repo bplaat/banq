c7bba27341e928d059790a07b64e3574ce63cbc5
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-03-03 09:46:23 +0100

    Updated the windows keypad text example

--- atm-client/keypad-test/keypad.c
+++ atm-client/keypad-test/keypad.c
@@ -10,112 +10,150 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define COM_FILE "COM4"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
-const char g_szClassName[] = "keypadTest";
-WNDCLASSEX wc;
-HWND hwnd;
-HANDLE hComm;
-char infoString[256] = "Connecting...";
-HFONT font;
+char className[] = "keypadTest";
+char windowTitle[] = "Keypad Serial Demo";
+char versionLabel[] = "v0.1";
+char comFile[] = "COM4";
+char fontName[] = "Tahoma";
+char footerLabel[] = "Made by Bastiaan van der Plaat";
+
+char infoLabel[256] = "Connecting...";
+HWND hwnd = NULL;
+HANDLE serialFile = NULL;
+HFONT largeFont = NULL;
+HFONT smallFont = NULL;
+
+int rand_range (int min, int max) {
+    return rand() % ((max - min) + 1) + min;
+}
 
 DWORD WINAPI serialReadThread(LPVOID lpParameter) {
-    char serialBuffer[256] = { 0 };
+    char serialBuffer[256];
     for (;;) {
         DWORD length = 0;
-        ReadFile(hComm, &serialBuffer, 255, &length, NULL);
+        ReadFile(serialFile, &serialBuffer, 255, &length, NULL);
         if (length > 0) {
             serialBuffer[length] = 0;
-            strcpy(infoString, serialBuffer);
+            strcpy(infoLabel, serialBuffer);
             InvalidateRect(hwnd, NULL, TRUE);
         }
     }
     return 0;
 }
 
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch(msg) {
-        case WM_CREATE:
-            font = CreateFont(48, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, "Arial");
+    if (msg == WM_CREATE) {
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        int width = WINDOW_WIDTH + (WINDOW_WIDTH - clientRect.right);
+        int height = WINDOW_HEIGHT + (WINDOW_HEIGHT - clientRect.bottom);
+        SetWindowPos(hwnd, HWND_TOP, (GetSystemMetrics(SM_CXSCREEN) - width) / 2, (GetSystemMetrics(SM_CYSCREEN) - height) / 2, width, height, SWP_SHOWWINDOW);
 
-            hComm = CreateFile(COM_FILE,               // Port name
-                      GENERIC_READ | GENERIC_WRITE, // Read/Write
-                      0,                            // No Sharing
-                      NULL,                         // No Security
-                      OPEN_EXISTING,                // Open existing port only
-                      0,                            // Non Overlapped I/O
-                      NULL);                        // Null for Comm Devices
+        largeFont = CreateFont(width * 10 / 100, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, fontName);
+        smallFont = CreateFont(width * 3 / 100, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, fontName);
 
-            if (hComm != INVALID_HANDLE_VALUE) {
-                DCB dcbSerialParams = { 0 };            // Initializing DCB structure
-                dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-                dcbSerialParams.BaudRate = CBR_9600;    // Setting BaudRate = 9600
-                dcbSerialParams.ByteSize = 8;           // Setting ByteSize = 8
-                dcbSerialParams.StopBits = ONESTOPBIT;  // Setting StopBits = 1
-                dcbSerialParams.Parity   = NOPARITY;    // Setting Parity = None
-                SetCommState(hComm, &dcbSerialParams);
+        serialFile = CreateFile(comFile, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
-                COMMTIMEOUTS timeouts = { 0 };
-                timeouts.ReadIntervalTimeout         = 50; // in milliseconds
-                timeouts.ReadTotalTimeoutConstant    = 50; // in milliseconds
-                timeouts.ReadTotalTimeoutMultiplier  = 10; // in milliseconds
-                timeouts.WriteTotalTimeoutConstant   = 50; // in milliseconds
-                timeouts.WriteTotalTimeoutMultiplier = 10; // in milliseconds
-                SetCommTimeouts(hComm, &timeouts);
+        if (serialFile != INVALID_HANDLE_VALUE) {
+            DCB dcbSerialParams = { 0 };
+            dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+            dcbSerialParams.BaudRate = CBR_9600;
+            dcbSerialParams.ByteSize = 8;
+            dcbSerialParams.StopBits = ONESTOPBIT;
+            dcbSerialParams.Parity = NOPARITY;
+            SetCommState(serialFile, &dcbSerialParams);
 
-                CreateThread(0, 0, serialReadThread, NULL, 0, NULL);
+            COMMTIMEOUTS timeouts = { 0 };
+            timeouts.ReadIntervalTimeout = 50;
+            timeouts.ReadTotalTimeoutConstant = 50;
+            timeouts.ReadTotalTimeoutMultiplier = 10;
+            timeouts.WriteTotalTimeoutConstant = 50;
+            timeouts.WriteTotalTimeoutMultiplier = 10;
+            SetCommTimeouts(serialFile, &timeouts);
 
-                strcpy(infoString, "Connected");
-            }
+            CreateThread(0, 0, serialReadThread, NULL, 0, NULL);
 
-            else {
-                strcpy(infoString, "No serial port");
-            }
-        break;
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            RECT rect;
-            GetClientRect(hwnd, &rect);
-            SelectObject(hdc, font);
-            SetBkMode(hdc, TRANSPARENT);
-            DrawText(hdc, infoString, -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-
-            EndPaint(hwnd, &ps);
+            strcpy(infoLabel, "Connected");
         }
-        break;
-        case WM_DESTROY:
-            DeleteObject(font);
-            CloseHandle(hComm);
-            DeleteObject(wc.hbrBackground);
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
+
+        else {
+            strcpy(infoLabel, "No serial port");
+        }
+
+        return 0;
     }
-    return 0;
+
+    else if (msg == WM_SIZE) {
+        int width = LOWORD(lParam);
+        DeleteObject(largeFont);
+        largeFont = CreateFont(width * 10 / 100, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, fontName);
+        DeleteObject(smallFont);
+        smallFont = CreateFont(width * 3 / 100, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, fontName);
+    }
+
+    else if (msg == WM_PAINT) {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        SetBkMode(hdc, TRANSPARENT);
+
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        int width = clientRect.right;
+        int height = clientRect.bottom;
+        int padding = width * 3 / 100;
+
+        SelectObject(hdc, smallFont);
+        SetTextAlign(hdc, TA_LEFT);
+        TextOut(hdc, padding, padding, windowTitle, strlen(windowTitle));
+
+        SetTextAlign(hdc, TA_RIGHT);
+        TextOut(hdc, width - padding, padding, versionLabel, strlen(versionLabel));
+
+        SelectObject(hdc, largeFont);
+        SetTextAlign(hdc, TA_CENTER);
+        TextOut(hdc, width / 2, height / 2 - width * 5 / 100, infoLabel, strlen(infoLabel));
+
+        SelectObject(hdc, smallFont);
+        SetTextAlign(hdc, TA_CENTER | TA_BOTTOM);
+        TextOut(hdc, width / 2, height - padding, footerLabel, strlen(footerLabel));
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    else if (msg == WM_DESTROY) {
+        DeleteObject(largeFont);
+        DeleteObject(smallFont);
+        CloseHandle(serialFile);
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    else {
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     srand(time(NULL));
 
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = CreateSolidBrush(RGB((rand() % 127) + 127, (rand() % 127) + 127, (rand() % 127) + 127));
-    wc.lpszMenuName  = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = CreateSolidBrush(RGB(rand_range(128, 255), rand_range(128, 255), rand_range(128, 255)));
+    wc.lpszClassName = className;
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
     RegisterClassEx(&wc);
 
-    hwnd = CreateWindow(g_szClassName, "Keypad Serial Demo", WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+    hwnd = CreateWindow(className, windowTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0) > 0) {
