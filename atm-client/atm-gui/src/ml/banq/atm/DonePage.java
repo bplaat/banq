ef56df4c73ec8d16d8079737a796217a8576e200
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-03-07 12:13:46 +0100

    Add an Arduino IO serial controller and a Java Swing Bank GUI

---
+++ atm-client/atm-gui/src/ml/banq/atm/DonePage.java
@@ -0,0 +1,36 @@
+package ml.banq.atm;
+
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DonePage extends Page {
+    private static final long serialVersionUID = 1;
+
+    public DonePage() {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        add(Box.createVerticalGlue());
+
+        JLabel titleLabel = new JLabel("Done");
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titleLabel.setFont(Fonts.HEADER);
+        add(titleLabel);
+
+        add(Box.createVerticalStrut(24));
+
+        JLabel messageLabel = new JLabel("Press any key on the keypad to go back to the welcome page...");
+        messageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        messageLabel.setFont(Fonts.NORMAL);
+        add(messageLabel);
+
+        add(Box.createVerticalGlue());
+    }
+
+    public void onKeypad(String key) {
+        Navigator.changePage(new WelcomePage());
+    }
+}
