f29e73f7838c4230d4f067a6990c65276586a8bd
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-04-19 07:44:23 +0200

    Update the build system to enable fat jar release

--- atm-client/atm-gui/build.sh
+++ atm-client/atm-gui/build.sh
@@ -1,1 +1,8 @@
-mvn clean compile exec:java
+#!/bin/bash
+# Run './build.sh' when you want to compile and run the Java Application
+# Run './build.sh release' when you want to compile and package to a release jar
+if [ "$1" = "release" ]; then
+    mvn clean compile package
+else
+    mvn clean compile exec:java
+fi
